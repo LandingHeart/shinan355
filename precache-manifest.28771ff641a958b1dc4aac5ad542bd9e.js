72e9db746a467356b2ced86b73741ba1e1bc7686
LandingHeart <land.liu0123@gmail.com>
2020-03-14 11:42:26 -0400

    Updates

---
+++ precache-manifest.28771ff641a958b1dc4aac5ad542bd9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc398f2ef0d9dadcc2e3eaf6b035fb2",
+    "url": "/shinan355/index.html"
+  },
+  {
+    "revision": "daa36523e1dfc39c6b75",
+    "url": "/shinan355/static/css/main.b645d88a.chunk.css"
+  },
+  {
+    "revision": "75742911612b425f39c7",
+    "url": "/shinan355/static/js/2.4de5ddc3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/shinan355/static/js/2.4de5ddc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa36523e1dfc39c6b75",
+    "url": "/shinan355/static/js/main.02b01e51.chunk.js"
+  },
+  {
+    "revision": "600a2746970b4fcdb1b0",
+    "url": "/shinan355/static/js/runtime-main.8423412e.js"
+  },
+  {
+    "revision": "5f32e0d47111029e6445086b566fd283",
+    "url": "/shinan355/static/media/img.5f32e0d4.png"
+  }
+]);
